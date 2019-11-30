a590fe642ac42253a3d619c86d3ef0bae1cab7ae
Tsai0819 <42505264+Tsai0819@users.noreply.github.com>
2019-11-30 20:15:06 +0800

    玩家打僵屍 僵屍打玩家

--- UnityProjectfore/Assets/Script/player.cs
+++ UnityProjectfore/Assets/Script/player.cs
@@ -8,12 +8,16 @@ public class player : MonoBehaviour
 
     public float Attack;
 
-    public AudioSource ZOM;
+    public AudioSource MU;
 
-    public AudioClip ZOMTWO;
+    public AudioClip playerMU;
+
+    public zombie zombieNOW;
+
+    int deadtime;
 
 
- 
+
 
     // Start is called before the first frame update
     void Start()
@@ -24,12 +28,44 @@ public class player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.E) && hp > 0)
+        {
+            Attacker();
+        }
+        if (hp <= 0 && deadtime < 1)
+        {
+            Dead();
+        }
     }
-    public void Attacker() 
+    public void Attacker()
     {
-        
+        MU.PlayOneShot(playerMU);
+
+        if (zombieNOW.hp <= 0)
+        {
+            if (zombieNOW.hp <= 0)
+            {
+                return;
+            }
+            zombieNOW.hp -= (Random.Range(0f, 10f));
+
+            if (zombieNOW.hp > 0)
+            {
+                Debug.Log("僵屍剩餘血量" + zombieNOW.hp);
+            }
+
+            else
+            {
+                Debug.Log("僵屍剩餘血量" + 0);
+            }
+        }
+
+    }
+    public void Dead()
+    {
+        Debug.Log("我死了");
+        deadtime = 1;
     }
 
-    
+
 }
--- UnityProjectfore/Assets/Script/zombie.cs
+++ UnityProjectfore/Assets/Script/zombie.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class zombie : MonoBehaviour
 {
+    public float hp;
+
+    public float Attack;
+
+    public AudioSource ZOM;
+
+    public AudioClip ZOMTWO;
+
+    public player playNOW;
+
+    int deadtime;
+
+    　
+
+
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.A)&&hp>0) 
+        {
+            Attacker();
+        }
+
+        if (hp<=0&& deadtime<1)
+        {
+            Dead();
+        }
+    }
+    public void Attacker()
+    {
+        ZOM.PlayOneShot(ZOMTWO);
+
+        if (playNOW.hp <= 0)
+        {
+            return;
+        }
+        playNOW.hp -= (Random.Range(0f, 10f));
+
+        if (playNOW.hp > 0)
+        {
+            Debug.Log("僵屍剩餘血量" + playNOW.hp);
+        }
+
+        else
+        {
+            Debug.Log("僵屍剩餘血量" + 0);
+        }
+    }
+    public void Dead() 
+    {
+        Debug.Log("我死了");
+        deadtime = 1;
     }
 }
