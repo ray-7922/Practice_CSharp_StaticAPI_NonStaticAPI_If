8ab74f1fffadbb9ffa851982e287d8aef1d99c18
Tsai0819 <42505264+Tsai0819@users.noreply.github.com>
2019-11-30 16:43:21 +0800

    飛天奇雞

---
+++ UnityProjectfore/Assets/Script/player.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class player : MonoBehaviour
+{
+    public float hp;
+
+    public float Attack;
+
+    public AudioSource ZOM;
+
+    public AudioClip ZOMTWO;
+
+
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Attacker() 
+    {
+        
+    }
+
+    
+}
